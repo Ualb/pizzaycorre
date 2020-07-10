e307e1a6dd69f8752548ff253e0864f5d513ab51
atlasfoo@win <jf_mejiar547@live.com>
2020-07-09 23:31:01 -0600

    finished layout functions

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//mostrar modal login
+document.addEventListener('DOMContentLoaded', ()=>{
+   document.querySelector('#login-btn').onclick = ()=>{
+       openModal();
+       return false;
+   } 
+   window.onclick = evt =>{
+       if(evt.target == document.querySelector("#login-modal")){
+           closeModal();
+       }
+   }
+   document.querySelector("#login-form").onsubmit = validateSubmit;
+})
+
+function openModal(){
+    var modal = document.querySelector("#login-modal");
+    modal.style.display = 'block'
+}
+
+function closeModal(){
+    var modal = document.querySelector("#login-modal");
+    modal.style.display = 'none';
+}
+
+function validateSubmit() {
+    if (document.querySelector("#user").value == "" || document.querySelector("#pswd").value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Rellene todos los campos',
+        })
+        return false;
+    }
+    
+}
